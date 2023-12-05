b2a76ca39d6afacf71e39c7164d07da9bdc1e102
ruwangakonara <ruwangathandulakonara@gmail.com>
2023-12-05 17:24:24 +0530

    sql navs conflict

--- app/views/worker/workernav.php
+++ app/views/worker/workernav.php
@@ -6,43 +6,44 @@
 </head>
 
 <body>
-    <div class="homenavbar">
-        <header>
-            <div class="logo">Errand</div>
-            <input type="checkbox" id="nav_check" hidden>
-            <nav>
-                <ul>
+<div class="homenavbar">
+    <header>
+        <div class="logo">Errand</div>
+        <input type="checkbox" id="nav_check" hidden>
+        <nav>
+            <ul>
 
-                    <li>
-                        <a href="<?= ROOT ?>/worker/home">Jobs</a>
-                    </li>
-                    <li>
-                        <a href="<?= ROOT ?>/worker/services">Services</a>
-                    </li>
-                </ul>
-            </nav>
-            <div class="icons">
-                <a href="<?= ROOT ?>/worker/messages"><img src="<?= ROOT ?>/assets/images/employer/msg.png" alt="Message"></a>
-                <a href="<?= ROOT ?>/worker/notifications"><img src="<?= ROOT ?>/assets/images/employer/belll.png" alt="notification"></a>
-                <div class="profile-dropdown" id="profile-dropdown">
-                    <a href="#"><img class="profile-icon" src="<?= ROOT ?>/assets/images/employer/prr.png" alt="Profile"></a>
-                    <div class="profile-menu" id="profile-menu">
-                        <a href="<?= ROOT ?>/worker/home">Home</a>
-                        <a href="">Dashboard</a>
-                        <a href="<?= ROOT ?>/worker/myjobs">My Jobs</a>
-                        <a href="<?= ROOT ?>/worker/messages">Message</a>
-                        <a href="<?= ROOT ?>/worker/workerprofile">Profile</a>
-                        <a href="<?= ROOT ?>/worker/tickets">Tickets</a>
-                        <a href="<?= ROOT ?>/home/signout">LogOut</a>
-                    </div>
+                <li>
+                    <a href="<?= ROOT ?>/worker/home">Jobs</a>
+                </li>
+                <li>
+                    <a href="<?= ROOT ?>/worker/services">Services</a>
+                </li>
+            </ul>
+        </nav>
+        <div class="icons">
+            <a href="<?= ROOT ?>/worker/messages"><img src="<?= ROOT ?>/assets/images/employer/msg.png" alt="Message"></a>
+            <a href="<?= ROOT ?>/worker/notifications"><img src="<?= ROOT ?>/assets/images/employer/belll.png" alt="notification"></a>
+            <div class="profile-dropdown" id="profile-dropdown">
+                <a href="#"><img class="profile-icon" src="<?= ROOT ?>/assets/images/employer/prr.png" alt="Profile"></a>
+                <div class="profile-menu" id="profile-menu">
+                    <a href="<?= ROOT ?>/worker/home">Home</a>
+                    <a href="">Dashboard</a>
+                    <a href="<?= ROOT ?>/worker/myjobs">My Jobs</a>
+                    <a href="<?= ROOT ?>/worker/messages">Message</a>
+                    <a href="<?= ROOT ?>/worker/workerprofile">Profile</a>
+                    <a href="<?= ROOT ?>/worker/tickets">Tickets</a>
+                    <a href="<?= ROOT ?>/home/signout">LogOut</a>
+
                 </div>
             </div>
-            <label for="nav_check" class="hamburger"></label>
+        </div>
+        <label for="nav_check" class="hamburger"></label>
 
 
-        </header>
-    </div>
-    <script src="<?= ROOT ?>/assets/js/employer/navlist.js"></script>
+    </header>
+</div>
+<script src="<?= ROOT ?>/assets/js/employer/navlist.js"></script>
 </body>
 
 </html>
