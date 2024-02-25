c5582a72cd850c29448ab2d71929b70118347e94
rlakmal <rruchiralakmal@gmail.com>
2024-02-26 01:29:43 +0530

    emp view job

---
+++ app/controllers/employer/ViewJob.php
@@ -0,0 +1,19 @@
+<?php
+
+class ViewJob extends Controller
+{
+    public function index($a = '', $b = '', $c = '')
+    {
+        $username = empty($_SESSION['USER']) ? 'User' : $_SESSION['USER']->email;
+        if ($username != 'User' && $_SESSION['USER']->status == 'employer') {
+            $job = new EmpPost;
+            $id = $_GET['id'];
+            $arr['id'] = $id;
+            $results = $job->where($arr);
+            // show($results);
+            $data['data'] = $results;
+
+            $this->view('employer/viewjob', $data);
+        }
+    }
+}
