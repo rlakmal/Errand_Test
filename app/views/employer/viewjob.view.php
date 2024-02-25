420a356ba00d696613a273d142d639a5294a6fd2
rlakmal <rruchiralakmal@gmail.com>
2024-02-26 01:29:43 +0530

    emp view job

---
+++ app/views/employer/viewjob.view.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?= ROOT ?>/assets/css/worker/requestjob.css">
+    <title>viewjob</title>
+</head>
+
+<body>
+    <?php include 'employernav.php' ?>
+    <?php include 'empfilter.php' ?>
+    <?php
+    if ($data) {
+        foreach ($data as $item) {
+            //show($data);
+    ?>
+
+            <div class="main-container4">
+                <div class="profile-container3">
+                    <div class="container-left">
+
+                        <img class="image" src="<?= ROOT ?>/assets/images/profileImages/<?php echo $item->profile_image  ?>" alt="">
+                        <h3 class="category">
+                            Category
+                        </h3>
+                        <h3 class="emp-name">
+                            <?php echo $item->name  ?>
+                        </h3>
+
+                        <img class="rates" src="<?= ROOT ?>/assets/images/worker/rates.png" alt="">
+
+                    </div>
+
+                    <div class="container-right">
+                        <div type="text" name="fullname" value="" class="title-line" readonly><?php echo $item->title  ?></div>
+                        <h3>
+                            Location
+                        </h3>
+                        <div type="text" name="city" value="" class="edit-gen" readonly><?php echo $item->city ?></div>
+                        <h3>
+                            Address
+                        </h3>
+                        <div type="text" name="address" value="" class="edit-gen" readonly><?php echo $item->address ?></div>
+                        <h3>
+                            Budget
+                        </h3>
+                        <div type="text" name="budget" value='' class="edit-gen" readonly>Rs <?php echo $item->budget ?> Per Day</div>
+                        <h3>
+                            Description
+                        </h3>
+                        <div type="text" name="description" value='' class="edit-gen" readonly><?php echo $item->description ?></div>
+                        <h3>
+                            Request Other Budget
+                        </h3>
+                        <form method="POST">
+                            <input name="newbudget" type="text" name="bargain" value='' class="edit-gen">
+                            <input type="hidden" name="id" value="<?php echo $item->id ?>">
+
+
+
+                            <div class="job_images">
+                                <img class="jobimage" src="<?= ROOT ?>/assets/images/jobimages/<?php echo $item->job_image  ?>" alt="">
+                                <img class="jobimage1" src="<?= ROOT ?>/assets/images/jobimages/<?php echo $item->job_image1  ?>" alt="">
+                            </div>
+
+                    </div>
+                </div>
+                <div class="index_bottom">
+                    <button type="submit" name="Rquest" value="Request" class="close-button">Request</button>
+                    </form>
+                    <a href="<?= ROOT ?>/employer/home"><button class="close-button">Back</button></a>
+                </div>
+            </div>
+
+    <?php
+
+        }
+    }
+
+    ?>
+
+</body>
+
+</html>
