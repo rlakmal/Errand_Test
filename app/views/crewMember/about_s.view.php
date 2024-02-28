c7bcc73dc4e1e0fedbb6b55bb47fcd14606c5beb
sahanthathsara1 <sahanthathsaramasakorala@gmail.com>
2024-02-28 09:59:59 +0530

    bout_s

---
+++ app/views/crewMember/about_s.view.php
@@ -0,0 +1,268 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Oswald:wght@500&display=swap" rel="stylesheet">
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Oswald:wght@500&family=Sevillana&display=swap" rel="stylesheet">
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Kalam:wght@700&display=swap" rel="stylesheet">
+
+   
+    <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="about.css">
+    <title>Document</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<style>
+body {font-family: Arial, Helvetica, sans-serif;}
+
+.navbar {
+  width: 100%;
+  background-color:transparent;
+  overflow: auto;
+}
+
+.navbar a {
+  float: left;
+  padding: 12px;
+  color: rgb(228, 127, 19);
+  text-decoration: none;
+  font-size: 30px;
+}
+
+.navbar a:hover {
+  background-color: #0d0101c5;
+}
+.navbar.logo{
+    border-radius: 50%;
+    float: right;
+}
+
+.active {
+  background-color: #e9eeec;
+}
+
+@media screen and (max-width: 500px) {
+  .navbar a {
+    float: none;
+    display: block;
+  }
+}
+.abc{
+    color: #555;
+}
+#image-container {
+    position:right top;
+    width: 300px;
+    height: 300px;
+    overflow: hidden;
+
+  }
+
+  #image-container img {
+    width: 30%;
+    height: 40%;
+    object-fit: fill;
+    position: right top;
+    border-radius: 40%;
+    border-style: groove;
+    border-top-right-radius:10% ;
+    opacity: 0%;
+
+    animation: fadeInOut 8s infinite, scaleInOut 8s infinite;
+  }
+  @keyframes fadeInOut {
+    0%, 100% {
+      opacity: 0;
+    }
+    55%, 75% {
+      opacity: 1;
+    }
+  }
+
+  @keyframes scaleInOut {
+    0%, 100% {
+      transform: scale(1);
+    }
+    50% {
+      transform: scale(1.1);
+    }
+  }
+  
+ 
+</style>
+<script src="script.js"></script>
+<body>
+    
+
+<section class="image-1">
+    <div class="navbar">
+        <img src=  "\images for errand\err.png" height="100px" width="100px" class="logo">
+          <a class="active" href="#"><i class="fa fa-fw fa-home"></i> Home</a> 
+          <a href="#"><i class="fa fa-fw fa-search"></i> Search</a> 
+          <a href="#"><i class="fa fa-fw fa-envelope"></i> Contact</a> 
+          <a href="#"><i class="fa fa-fw fa-user"></i> Login</a>
+          </div>
+    <p>ANY JOB...</p>
+    <p>ANY TIME...</p>
+    <p>ANY WHERE....</p>
+    <table>
+        <tr>
+          <td>
+            <img src="https://media.newstrack.in/uploads/career-news-info/jobs/Mar/22/big_thumb/dddj_5e7792020d5dd.png" alt="Image 1" class="tableimg">
+            <pre class="description">  <b>  Post your job</b>
+  Tell us what you need.
+  It's FREE to post a job</pre>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <img src="https://thumbs.dreamstime.com/z/sale-tag-ice-cream-roller-coaster-icons-set-megaphone-smile-face-fireworks-stars-signs-vector-simple-yellow-speech-bubbles-218259025.jpg" alt="Image 2" class="tableimg">
+            <pre class="description">      <b>Review offers</b>
+  Get offers from trusted
+  service providers and view
+  their profiles</pre>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <img src="https://th.bing.com/th/id/OIP.TZBxYBrgZRooGIp3Hf07SAHaH0?rs=1&pid=ImgDetMain" alt="img3" class="tableimg">
+            <pre class="description">       <b>Get it done</b>
+  Choose the right person 
+  for your job and get it done.</pre>
+          </td>
+        </tr>
+      </table> 
+    <pre class="intro">New way to find people to 
+do your jobs. If you are 
+one of those thousands of
+people whostruggle to find people to
+do Jobs or struggle to find jobs, 
+THIS IS FOR YOU!</pre>
+<div class="button-container">
+    <br><button class="btn">Want to Work</button><br><br>
+    <button class="btn">Want to Hire</button>
+  </div>
+  </pre>
+  
+ 
+       
+        <div class="split right" id="image-container">
+            <img src="\images for errand\two-handymen-making-measures.jpg" alt="Image 1" class="intropic">
+            <img src="\images for errand\two-handymen-making-measures.jpg" alt="Image 1" class="intropic">
+            <img src="\images for errand\young-craftsman-building-house.jpg" alt="Image 2" class="intropic">
+            <img src="images for errand\male-electrician-works-switchboard-with-electrical-connecting-cable.jpg" alt="Image 2" class="intropic">
+           
+           </div>
+       
+        
+</section>
+
+<section class="image-2"><bR>
+    <h1 class="underline-animation">See how others are getting ERRAND services</h1><br><br><br>
+    <div class="tile-container">
+        <div class="tile">
+            <img src="https://clipground.com/images/constructing-clipart-10.jpg" alt="Image 1">
+            <pre class="tiletext">Example Text 1 </pre>
+                                                
+        </div>
+
+        <div class="tile">
+          <img src="https://th.bing.com/th/id/R.8b639f4d76532ec8558f12ef9d5c5153?rik=iOB885G1tOK4RQ&pid=ImgRaw&r=0" alt="Image 1">
+          <pre class="tiletext">Example Text 1</pre>
+      </div>
+
+      <div class="tile">
+        <img src="https://th.bing.com/th/id/R.bb333c76bfafecab92ee44e4f12a1ae2?rik=%2fmoiq5UkfNczlg&pid=ImgRaw&r=0" alt="Image 1">
+        <pre class="tiletext">Example Text 1</pre>
+    </div>
+
+    <div class="tile">
+      <img src="https://thumbs.dreamstime.com/b/woman-repairing-sink-sitting-bathroom-spanner-vector-flat-design-illustration-square-layout-66321106.jpg" alt="Image 1">
+      <pre class="tiletext">Example Text 1</pre>
+  </div>
+
+  <div class="tile">
+    <img src="https://th.bing.com/th/id/R.7d90efdfe97d54b79f85482fadc9c166?rik=vSx2oT%2bCm3doyQ&riu=http%3a%2f%2fwww.clker.com%2fcliparts%2f1%2f5%2f2%2f7%2f15137920851570870547animated-house-painter.hi.png&ehk=MD2U0v5CIIsJxOoNTbm6Yk6SVGthsZwdbm6kJp67Sy4%3d&risl=&pid=ImgRaw&r=0" alt="Image 1">
+    <pre class="tiletext">Example Text 1</pre>
+</div>
+    </div><br><br>
+
+    
+    
+        
+</section>
+
+<section class="image-3">
+    <div class="container">
+        <img src="https://images.unsplash.com/photo-1504917595217-d4dc5ebe6122?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80" alt="Snow">
+        <div class="centered">
+          <div class="text-block">Walking with a friend in the dark is better than walking alone in the light</div>
+        </div>
+      </div>
+</section>
+
+
+<footer class="footer">
+    <div class="container">
+     <div class="row">
+       <div class="footer-col">
+         <h4>company</h4>
+         <ul>
+           <li><a href="#">about us</a></li>
+           <li><a href="#">our services</a></li>
+           <li><a href="#">privacy policy</a></li>
+           <li><a href="#">affiliate program</a></li>
+         </ul>
+       </div>
+       <div class="footer-col">
+         <h4>get help</h4>
+         <ul>
+           <li><a href="#">FAQ</a></li>
+           <li><a href="#">shipping</a></li>
+           <li><a href="#">returns</a></li>
+           <li><a href="#">order status</a></li>
+           <li><a href="#">payment options</a></li>
+         </ul>
+       </div>
+       <div class="footer-col">
+         <h4>online shop</h4>
+         <ul>
+           <li><a href="#">watch</a></li>
+           <li><a href="#">bag</a></li>
+           <li><a href="#">shoes</a></li>
+           <li><a href="#">dress</a></li>
+         </ul>
+       </div>
+       <div class="footer-col">
+         <h4>follow us</h4>
+         <div class="social-links">
+           <a href="#"><i class="fab fa-facebook-f"></i></a>
+           <a href="#"><i class="fab fa-twitter"></i></a>
+           <a href="#"><i class="fab fa-instagram"></i></a>
+           <a href="#"><i class="fab fa-linkedin-in"></i></a>
+         </div>
+       </div>
+     </div>
+    </div>
+ </footer>
+
+
+</body>
+</html> 
