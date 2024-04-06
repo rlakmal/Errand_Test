8a924fb1d22b7b62dfc6e9b282440743cd63a9e8
ruwangakonara <ruwangathandulakonara@gmail.com>
2024-04-06 09:36:58 +0530

    rebase conflict 4/6 2

--- app/views/employer/myworkerreq.view.php
+++ app/views/employer/myworkerreq.view.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -14,7 +15,6 @@
 
 <body>
 
-<<<<<<< HEAD
 
 <?php include 'employernav2.php' ?>
 <?php include 'myjobsidebar.php' ?>
@@ -45,17 +45,6 @@
                         //show($item);
                         $no++;
                         ?>
-=======
-    <?php include 'employernav2.php' ?>
-    <?php include 'myjobsidebar.php' ?>
-    <diV class="set_margin">
-        <section id="main" class="main">
-
-            <h2>Your Request to Workers</h2>
-            <div class="scrollable-table">
-                <table class="table">
-                    <thead>
->>>>>>> eda85ab (ajax popup ui)
                         <tr>
                             <td class="proimage"><?php echo $no ?></td>
 
@@ -85,7 +74,6 @@
                                     <td class="thcancel"><button type="submit" name="Cancel" value="Cancel" class="cancelbutton">Cancel Request</button></td>
                                 </form>
 
-<<<<<<< HEAD
                                 <?php
                             } elseif ($item->status == 'Requested') {
                                 ?>
@@ -109,80 +97,10 @@
                 ?>
                 </tbody>
             </table>
-=======
-                                    <td class="proimage">
-                                        <img class="image" src="<?= ROOT ?>/assets/images/profileImages/<?php echo $_SESSION['USER']->profile_image ?>" alt="profile image">
-                                        <!-- </td>
-                                        <td class="proname"> -->
-                                        <a class="wkname" href="<?= ROOT ?>/employer/workerprof?id=<?php echo $item->worker_id ?>"><?php echo $item->worker_name ?></a>
-                                    </td>
-                                    <td><?php echo $item->title ?></td>
-                                    <td>RS <?php echo $item->budget ?>/=</td>
-                                    <td><?php echo $item->city ?></td>
-                                    <td><button class="<?php if ($item->status == "Pending" || $item->status == "Accepted") {
-                                                            echo "pendingbutton";
-                                                        } elseif ($item->status == "Rejected" || $item->status == "Requested") {
-                                                            echo "rejectedbutton";
-                                                        } else {
-                                                            echo "expirebutton";
-                                                        }
-
-                                                        ?>"><?php echo $item->status ?></button></td>
-                                    <?php
-                                    if ($item->status == 'Pending') {
-                                    ?>
-                                        <form method="POST">
-                                            <input type="hidden" name="id" value="<?php echo $item->id ?>">
-                                            <td class="thcancel"><button type="submit" name="Cancel" value="Cancel" class="cancelbutton">Cancel Request</button></td>
-                                        </form>
-
-                                    <?php
-                                    } elseif ($item->status == 'Requested') {
-                                    ?>
-                                        <!-- <form method="POST"> -->
-                                        <input type="hidden" name="id <?php echo $item->id ?>" value="<?php echo $item->id ?>">
-                                        <td class="thcancel">
-                                            <button id="viewRequestbtn_<?php echo $item->id ?>" name="viewRequest" value="viewRequest" onclick="openEdit( <?= $item->id ?> )" class="viewbutton">View Request</button>
-                                        </td>
-                                        <!-- </form> -->
-                                    <?php
-                                    } else {
-                                    ?>
-                                        <td class="thcancel"></td>
-                                    <?php
-                                    }
-                                    ?>
-                                </tr>
-                        <?php
-                            }
-                        }
-                        ?>
-                    </tbody>
-                </table>
-
-            </div>
-            <div class="pop-view">
-                <form id="pop-form" method="POST">
-                    <div id="pop-header">Worker want to negotiate your budget</div>
-                    <h4 id="newBudgetLabel">New Budget: </h4>
-                    <div class="pop-btn">
-                        <button class="pop-accept">Accept Offer</button>
-                        <button class="pop-reject">Reject</button>
-                    </div>
-                </form>
-
-            </div>
-
-
-
-            <div id="pop-overlay" class="pop-overlay"></div>
-    </div>
->>>>>>> eda85ab (ajax popup ui)
 
         </div>
 
     </section>
-<<<<<<< HEAD
 
 </div>
 <div class="pop-view">
@@ -224,27 +142,6 @@
         popupEdit.classList.remove("open-pop-view");
         overlay1.classList.remove("pop-overlay-active");
     }
-=======
-
-    </diV>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-
-    <script>
-        let popupEdit = document.querySelector(".pop-view");
-        let overlay1 = document.getElementById("pop-overlay");
-        current_id = 0;
-
-        function openEdit(id) {
-            current_id = id;
-            popupEdit.classList.add("open-pop-view");
-            overlay1.classList.add("pop-overlay-active");
-        }
-
-        function closeEdit() {
-            popupEdit.classList.remove("open-pop-view");
-            overlay1.classList.remove("pop-overlay-active");
-        }
->>>>>>> eda85ab (ajax popup ui)
 
 
 
@@ -260,7 +157,6 @@
                 }
                 console.log(data);
 
-<<<<<<< HEAD
                 $.ajax({
                     type: "POST",
                     url: "<?= ROOT ?>/employer/view_request",
@@ -274,19 +170,6 @@
                         $("#pop-hidden-id").val(newData.id);
                         $("#pop-hidden-budget").val(newData.newbudget);
                         try {} catch (error) {
-=======
-                    $.ajax({
-                        type: "POST",
-                        url: "<?= ROOT ?>/employer/view_request",
-                        data: data,
-                        cache: false,
-                        success: function(res) {
-                            //console.log("Response:", res);
-                            newData = JSON.parse(res);
-                            console.log(newData);
-                            $("#newBudgetLabel").text("New Budget: " + newData.newbudget + " Per Day");
-                            try {} catch (error) {
->>>>>>> eda85ab (ajax popup ui)
 
                         }
                     },
@@ -298,19 +181,12 @@
             })
 
         })
-<<<<<<< HEAD
 
     })
 </script>
-=======
-    </script>
->>>>>>> eda85ab (ajax popup ui)
 
 </body>
 
 
-<<<<<<< HEAD
 
-=======
->>>>>>> eda85ab (ajax popup ui)
 </html>
