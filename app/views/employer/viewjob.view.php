5b094d14b9deeb3488ec4b437777815c08afef20
ruwangakonara <ruwangathandulakonara@gmail.com>
2024-04-06 10:27:30 +0530

    rebase conflict 4/6 12

--- app/views/employer/viewjob.view.php
+++ app/views/employer/viewjob.view.php
@@ -45,119 +45,119 @@
 </head>
 
 <body>
-    <?php include 'employernav.php' ?>
-    <?php include 'empfilter.php' ?>
-    <?php
-    if ($data) {
-        foreach ($data as $item) {
-            //show($data);
-    ?>
+<?php include 'employernav.php' ?>
+<?php include 'empfilter.php' ?>
+<?php
+if ($data) {
+    foreach ($data as $item) {
+        //show($data);
+        ?>
 
-            <div class="main-container4">
-                <div class="profile-container3">
-                    <div class="container-left">
+        <div class="main-container4">
+            <div class="profile-container3">
+                <div class="container-left">
 
-                        <img class="image" src="<?= ROOT ?>/assets/images/profileImages/<?php echo $item->profile_image  ?>" alt="">
-                        <h3 class="category">
-                            Category
-                        </h3>
-                        <h3 class="emp-name">
-                            <?php echo $item->name  ?>
-                        </h3>
+                    <img class="image" src="<?= ROOT ?>/assets/images/profileImages/<?php echo $item->profile_image  ?>" alt="">
+                    <h3 class="category">
+                        Category
+                    </h3>
+                    <h3 class="emp-name">
+                        <?php echo $item->name  ?>
+                    </h3>
 
-                        <img class="rates" src="<?= ROOT ?>/assets/images/worker/rates.png" alt="">
+                    <img class="rates" src="<?= ROOT ?>/assets/images/worker/rates.png" alt="">
 
-                    </div>
-
-                    <div class="container-right">
-                        <div type="text" name="fullname" value="" class="title-line" readonly><?php echo $item->title  ?></div>
-                        <h3>
-                            Location
-                        </h3>
-                        <div type="text" name="city" value="" class="edit-gen" readonly><?php echo $item->city ?></div>
-                        <h3>
-                            Address
-                        </h3>
-                        <div type="text" name="address" value="" class="edit-gen" readonly><?php echo $item->address ?></div>
-                        <h3>
-                            Budget
-                        </h3>
-                        <div type="text" name="budget" value='' class="edit-gen" readonly>Rs <?php echo $item->budget ?> Per Day</div>
-                        <h3>
-                            Description
-                        </h3>
-                        <div type="text" name="description" value='' class="edit-gen" readonly><?php echo $item->description ?></div>
-                        <div class="job_images">
-                            <img class="jobimage1" src="<?= ROOT ?>/assets/images/jobimages/<?php echo $item->job_image  ?>" alt="" onclick="openModal(this.src,0)">
-                            <img class="jobimage1" src="<?= ROOT ?>/assets/images/jobimages/<?php echo $item->job_image1  ?>" alt="" onclick="openModal(this.src,1)">
-                        </div>
-
-                    </div>
                 </div>
-                <div class="index_bottom">
-                    <a href="<?= ROOT ?>/employer/home"><button class="close-button">Back</button></a>
+
+                <div class="container-right">
+                    <div type="text" name="fullname" value="" class="title-line" readonly><?php echo $item->title  ?></div>
+                    <h3>
+                        Location
+                    </h3>
+                    <div type="text" name="city" value="" class="edit-gen" readonly><?php echo $item->city ?></div>
+                    <h3>
+                        Address
+                    </h3>
+                    <div type="text" name="address" value="" class="edit-gen" readonly><?php echo $item->address ?></div>
+                    <h3>
+                        Budget
+                    </h3>
+                    <div type="text" name="budget" value='' class="edit-gen" readonly>Rs <?php echo $item->budget ?> Per Day</div>
+                    <h3>
+                        Description
+                    </h3>
+                    <div type="text" name="description" value='' class="edit-gen" readonly><?php echo $item->description ?></div>
+                    <div class="job_images">
+                        <img class="jobimage1" src="<?= ROOT ?>/assets/images/jobimages/<?php echo $item->job_image  ?>" alt="" onclick="openModal(this.src,0)">
+                        <img class="jobimage1" src="<?= ROOT ?>/assets/images/jobimages/<?php echo $item->job_image1  ?>" alt="" onclick="openModal(this.src,1)">
+                    </div>
+
                 </div>
             </div>
+            <div class="index_bottom">
+                <a href="<?= ROOT ?>/employer/home"><button class="close-button">Back</button></a>
+            </div>
+        </div>
 
-    <?php
+        <?php
 
+    }
+}
+
+?>
+<div id="myModal" class="modal">
+    <img class="modal-content" id="modalImage">
+</div>
+<script>
+    let currentIndex = 0;
+    const images = document.querySelectorAll('.jobimage1');
+    console.log(currentIndex);
+    const modalImage = document.getElementById('modalImage');
+
+    function openModal(src, index) {
+        currentIndex = index;
+        modalImage.src = src;
+        document.getElementById('myModal').style.display = 'flex';
+        document.addEventListener('keydown', handleKeyPress);
+    }
+
+    function closeModal() {
+        document.getElementById('myModal').style.display = 'none';
+        document.removeEventListener('keydown', handleKeyPress);
+    }
+
+    function handleKeyPress(event) {
+        switch (event.key) {
+            case 'ArrowRight':
+                showNextImage();
+                break;
+            case 'ArrowLeft':
+                showPreviousImage();
+                break;
+            case 'Escape':
+                closeModal();
+                break;
+            default:
+                break;
         }
     }
 
-    ?>
-    <div id="myModal" class="modal">
-        <img class="modal-content" id="modalImage">
-    </div>
-    <script>
-        let currentIndex = 0;
-        const images = document.querySelectorAll('.jobimage1');
-        console.log(currentIndex);
-        const modalImage = document.getElementById('modalImage');
+    function showNextImage() {
+        currentIndex = (currentIndex + 1) % images.length;
+        modalImage.src = images[currentIndex].src;
+    }
 
-        function openModal(src, index) {
-            currentIndex = index;
-            modalImage.src = src;
-            document.getElementById('myModal').style.display = 'flex';
-            document.addEventListener('keydown', handleKeyPress);
+    function showPreviousImage() {
+        currentIndex = (currentIndex - 1 + images.length) % images.length;
+        modalImage.src = images[currentIndex].src;
+    }
+
+    window.onclick = function(event) {
+        if (event.target == document.getElementById('myModal')) {
+            closeModal();
         }
-
-        function closeModal() {
-            document.getElementById('myModal').style.display = 'none';
-            document.removeEventListener('keydown', handleKeyPress);
-        }
-
-        function handleKeyPress(event) {
-            switch (event.key) {
-                case 'ArrowRight':
-                    showNextImage();
-                    break;
-                case 'ArrowLeft':
-                    showPreviousImage();
-                    break;
-                case 'Escape':
-                    closeModal();
-                    break;
-                default:
-                    break;
-            }
-        }
-
-        function showNextImage() {
-            currentIndex = (currentIndex + 1) % images.length;
-            modalImage.src = images[currentIndex].src;
-        }
-
-        function showPreviousImage() {
-            currentIndex = (currentIndex - 1 + images.length) % images.length;
-            modalImage.src = images[currentIndex].src;
-        }
-
-        window.onclick = function(event) {
-            if (event.target == document.getElementById('myModal')) {
-                closeModal();
-            }
-        };
-    </script>
+    };
+</script>
 </body>
 
 </html>
