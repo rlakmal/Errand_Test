a3bcbf3c4a7e209d13615194d567c4a5742e3073
rlakmal <rruchiralakmal@gmail.com>
2024-04-08 16:51:06 +0530

    payment

---
+++ app/controllers/employer/ReviewRequest.php
@@ -0,0 +1,19 @@
+<?php
+
+class ReviewRequest extends Controller
+{
+    public function index($a = '', $b = '', $c = '')
+    {
+        $username  = empty($_SESSION['USER']) ? 'User' : $_SESSION['USER']->email;
+        if ($username != 'User' && $_SESSION['USER']->status == 'employer') {
+            $accepted_jobs = new AcceptedJobs;
+            $emp_id = $_SESSION['USER']->id;
+            $arr['emp_id'] = $emp_id;
+            $arr['payment_stat'] = "paid";
+            $results = $accepted_jobs->where($arr);
+            $data['data'] = $results;
+            show($data);
+            $this->view('employer/reviewrequest', $data);
+        }
+    }
+}
