ce562778731e8b3593782f67a2a1d2c263e9c021
ruwangakonara <ruwangathandulakonara@gmail.com>
2024-04-16 17:25:44 +0530

    rebase conflict 6

--- app/controllers/employer/PostedjobsRequest.php
+++ app/controllers/employer/PostedjobsRequest.php
@@ -18,11 +18,9 @@ class PostedjobsRequest extends Controller
                 unset($_POST['id']);
                 unset($_POST['Accept']);
                 $_POST['budget'] = $budegt;
-
                 $_POST['payment_stat'] = "unpaid";
                 $_POST['type'] = "worker";
                 //show($_POST);
-
                 $accepted_jobs->insert($_POST);
 
                 redirect('employer/postedjobsrequest');
