be6118da30652e07bd87b0f91cc67f6f93abdf43
rlakmal <rruchiralakmal@gmail.com>
2024-04-19 18:04:05 +0530

    jobmap css

--- app/views/worker/viewjobmap.view.php
+++ app/views/worker/viewjobmap.view.php
@@ -5,7 +5,24 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>JOb Map</title>
+    <style>
+        .mapdetails {
+            display: flex;
+            flex-direction: column;
+        }
 
+        .mapdetails h1 {
+            display: flex;
+            justify-content: center;
+            flex-direction: row;
+        }
+
+        #map {
+            height: 600px;
+            width: 75%;
+            left: 20%;
+        }
+    </style>
 </head>
 <?php include 'workernav.php' ?>
 <?php include 'workerfilter.php' ?>
