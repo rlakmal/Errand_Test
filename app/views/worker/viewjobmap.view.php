70247277fc4acac60ca9f42368359edbf02a908b
rlakmal <rruchiralakmal@gmail.com>
2024-04-19 20:04:19 +0530

    jobmap ok

--- app/views/worker/viewjobmap.view.php
+++ app/views/worker/viewjobmap.view.php
@@ -12,6 +12,9 @@
         }
 
         .mapdetails h1 {
+            margin-left: 12%;
+            margin-bottom: 2%;
+            font-size: 18px;
             display: flex;
             justify-content: center;
             flex-direction: row;
@@ -30,49 +33,57 @@
 <body>
 
     <div class="mapdetails">
-        <h1>Job Locations Map</h1>
+        <h1>Job Locations Map(Click to request job)</h1>
         <div id="map"></div>
     </div>
     <script>
         function initMap() {
+            if (navigator.geolocation) {
 
-            var map = new google.maps.Map(document.getElementById("map"), {
-                zoom: 10,
-                center: {
-                    lat: 6.882842411689467,
-                    lng: 79.89451365555615
-                },
-                gestureHandling: "none",
-                zoomControl: false
-            });
+                navigator.geolocation.getCurrentPosition(function(position) {
+                    var userLocation = {
+                        lat: position.coords.latitude,
+                        lng: position.coords.longitude
+                    };
+                    console.log(userLocation);
 
-            var xhr = new XMLHttpRequest();
-            xhr.open("GET", "<?= ROOT ?>/worker/fetchlocations", true);
-            xhr.onreadystatechange = function() {
-                if (xhr.readyState === XMLHttpRequest.DONE) {
-                    if (xhr.status === 200) {
-                        var locations = JSON.parse(xhr.responseText);
-                        console.log(locations);
-                        locations.forEach(function(location) {
-                            var marker = new google.maps.Marker({
-                                position: {
-                                    lat: parseFloat(location.latitude),
-                                    lng: parseFloat(location.longitude)
-                                },
-                                map: map,
-                                jobId: location.job_id
-                            });
-                            marker.addListener("click", function() {
-                                window.location.href = "<?= ROOT ?>/worker/requestjob?id=" + this.jobId;
-                            });
-                        });
+                    var map = new google.maps.Map(document.getElementById("map"), {
+                        zoom: 10,
+                        center: userLocation, // Use user's current location as the center
+                        gestureHandling: "none",
+                        zoomControl: false
+                    });
 
-                    } else {
-                        console.error("Failed to retrieve locations: " + xhr.status);
-                    }
-                }
-            };
-            xhr.send();
+                    var xhr = new XMLHttpRequest();
+                    xhr.open("GET", "<?= ROOT ?>/worker/fetchlocations", true);
+                    xhr.onreadystatechange = function() {
+                        if (xhr.readyState === XMLHttpRequest.DONE) {
+                            if (xhr.status === 200) {
+                                var locations = JSON.parse(xhr.responseText);
+                                locations.forEach(function(location) {
+                                    var marker = new google.maps.Marker({
+                                        position: {
+                                            lat: parseFloat(location.latitude),
+                                            lng: parseFloat(location.longitude)
+                                        },
+                                        map: map,
+                                        jobId: location.job_id
+                                    });
+                                    marker.addListener("click", function() {
+                                        window.location.href = "<?= ROOT ?>/worker/requestjob?id=" + this.jobId;
+                                    });
+                                });
+
+                            } else {
+                                console.error("Failed to retrieve locations: " + xhr.status);
+                            }
+                        }
+                    };
+                    xhr.send();
+                });
+            } else {
+                console.error("Geolocation is not supported by this browser.");
+            }
         }
     </script>
     <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA6GOvTVdWC3aNfI8Jg4gOkyk74hiOB0RE&libraries=places&callback=initMap"></script>
