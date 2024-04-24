b91bd3c38585a55d5ba438378d38bb8d0bd2027d
sahanthathsara1 <sahanthathsaramasakorala@gmail.com>
2024-04-24 14:39:13 +0530

    about form

---
+++ app/views/crewMember/formtesting.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+    * {
+        padding: 0;
+        margin: 0;
+        color: #1a1f36;
+        box-sizing: border-box;
+        word-wrap: break-word;
+        font-family: -apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica Neue,Ubuntu,sans-serif;
+      }
+      body {
+          min-height: 100%;
+          background-color: #ffffff;
+      }
+      h1 {
+          letter-spacing: -1px;
+      }
+      a {
+        color: #5469d4;
+        text-decoration: unset;
+      }
+      .login-root {
+          background: #fff;
+          display: flex;
+          width: 100%;
+          min-height: 100vh;
+          overflow: hidden;
+      }
+      .loginbackground {
+          min-height: 692px;
+          position: fixed;
+          bottom: 0;
+          left: 0;
+          right: 0;
+          top: 0;
+          z-index: 0;
+          overflow: hidden;
+      }
+      .flex-flex {
+          display: flex;
+      }
+      .align-center {
+        align-items: center; 
+      }
+      .center-center {
+        align-items: center;
+        justify-content: center;
+      }
+      .box-root {
+          box-sizing: border-box;
+      }
+      .flex-direction--column {
+          -ms-flex-direction: column;
+          flex-direction: column;
+      }
+      .loginbackground-gridContainer {
+          display: -ms-grid;
+          display: grid;
+          -ms-grid-columns: [start] 1fr [left-gutter] (86.6px)[16] [left-gutter] 1fr [end];
+          grid-template-columns: [start] 1fr [left-gutter] repeat(16,86.6px) [left-gutter] 1fr [end];
+          -ms-grid-rows: [top] 1fr [top-gutter] (64px)[8] [bottom-gutter] 1fr [bottom];
+          grid-template-rows: [top] 1fr [top-gutter] repeat(8,64px) [bottom-gutter] 1fr [bottom];
+          justify-content: center;
+          margin: 0 -2%;
+          transform: rotate(-12deg) skew(-12deg);
+      }
+      .box-divider--light-all-2 {
+          box-shadow: inset 0 0 0 2px #e3e8ee;
+      }
+      .box-background--blue {
+          background-color: #5469d4;
+      }
+      .box-background--white {
+        background-color: #ffffff; 
+      }
+      .box-background--blue800 {
+          background-color: #212d63;
+      }
+      .box-background--gray100 {
+          background-color: #e3e8ee;
+      }
+      .box-background--cyan200 {
+          background-color: #7fd3ed;
+      }
+      .padding-top--64 {
+        padding-top: 64px;
+      }
+      .padding-top--24 {
+        padding-top: 24px;
+      }
+      .padding-top--48 {
+        padding-top: 48px;
+      }
+      .padding-bottom--24 {
+        padding-bottom: 24px;
+      }
+      .padding-horizontal--48 {
+        padding: 48px;
+      }
+      .padding-bottom--15 {
+        padding-bottom: 15px;
+      }
+      
+      
+      .flex-justifyContent--center {
+        -ms-flex-pack: center;
+        justify-content: center;
+      }
+      
+      
+  
+</style>
+</head>
+<body>
+    
+</body>
+</html>
