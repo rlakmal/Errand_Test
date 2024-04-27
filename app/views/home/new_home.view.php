b9e6a0b4cb3f6a6931176f18ade19f367e47015b
Siraj Preena <sirajpreena311@gmail.com>
2024-04-27 18:30:37 +0530

    code refactor

--- app/views/home/new_home.view.php
+++ app/views/home/new_home.view.php
@@ -10,69 +10,8 @@
 </head>
 
 <body>
-    <!-- <div class="container">
-
-        // Navigation Bar
-        <div class="homenavbar">
-            <header>
-                <div class="logo">Errand</div>
-                <input type="checkbox" id="nav_check" hidden>
-                <nav>
-                    <ul>
-                        <li>
-                            <a href="#" class="active">Home</a>
-                        </li>
-                        <li>
-                            <a href="#">About</a>
-                        </li>
-                        <li>
-                            <a href="#" class="active">Login</a>
-                        </li>
-                        <li>
-                            <a href="#" class="active">Register As Worker</a>
-                        </li>
-
-                    </ul>
-                </nav>
-                <label for="nav_check" class="hamburger"></label>
-            </header>
-        </div>
-        // <script src="assets/homenavbar.js"></script>
-        <script src="<?= ROOT ?>/assets/js/home/new_home_navbar.js"></script>
-        
-        // Main Section 
-        <main class="main-section">
-
-            <div class="hero-section">
-                <div class="text-area">
-                    <h1>Hire<br>Workers.<br>or Get<br>Hired.</h1>
-                    <p>Looking for a laborer or worker for get your job done.<br>
-                        Looking for a job or work to do.<br>
-                        <b class="primary">Errand</b> got you covered.<br>
-                        <b>Now it's just a matter of minutes.</b>
-                    </p>
-
-                    <div class="btn-contianer">
-                        <button class="reg-btn">Register as Customer</button>
-                        <button class="reg-btn">Register as Worker</button>
-                    </div>
-                
-                </div>
-
-                
-                <div class="image">
-                    <div class="image-container">
-                        <img src="assets/images/signin-up/painter.jpg" alt="We can't show you now">
-                    </div>
-                </div>
-            </div>
-            
-        </main>
-        
-    </div> -->
-
     <!-- NavBar -->
-    <!-- <div class="homenavbar">
+    <div class="homenavbar">
         <header>
             <div class="logo">Errand</div>
             <input type="checkbox" id="nav_check" hidden>
@@ -95,10 +34,10 @@
             </nav>
             <label for="nav_check" class="hamburger"></label>
         </header>
-    </div> -->
-    <!-- <script src="assets/homenavbar.js"></script> -->
+    </div>
+    <script src="assets/homenavbar.js"></script>
     <!-- End of NavBar -->
-    <?php include 'homenavbar.php' ?>
+    <!-- <?php include 'homenavbar.php' ?> -->
     <div class="main-container">
         
         <!-- Hero Section -->
